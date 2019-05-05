576bd74b27fb00f25bea0043520c7b2ebbcb9396
Hassnian <hassnianxd@gmail.com>
2019-05-05 14:59:58 +0200

    Updates

---
+++ precache-manifest.d3ecdfd079bd12a5323ec4bed66e610d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a38ee850c873481f544c27e1bb69c2",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "a533f33506a49cdea2ec",
+    "url": "/pokemon/static/css/main.855f5eee.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/pokemon/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "a533f33506a49cdea2ec",
+    "url": "/pokemon/static/js/main.be7c2020.chunk.js"
+  },
+  {
+    "revision": "b04917ec47d1c084d1e9",
+    "url": "/pokemon/static/js/runtime~main.0909cbae.js"
+  }
+]);
